6170f820b708e785696e65cc6e82a1abe7fffb5d
faizadie30 <faiz.adie30@gmail.com>
2024-04-21 02:02:53 +0700

    add migration table clubs and customer_club_members

---
+++ database/migrations/2024_04_21_011905_create_clubs_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('clubs', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->text('logo')->nullable();
+      $table->text(
+        'banner'
+      )->nullable();
+      $table->string(
+        'place_name'
+      )->nullable();
+      $table
+        ->foreignId('country_id')
+        ->constrained('international_countries')
+        ->references('id')
+        ->onDelete('cascade');
+      $table->string(
+        'country_name'
+      )->nullable();
+      $table->bigInteger('state_id');
+      $table->string(
+        'state_name'
+      )->nullable();
+      $table->bigInteger('city_id');
+      $table->string(
+        'city_name'
+      )->nullable();
+      $table->text(
+        'address'
+      )->nullable();
+      $table->text(
+        'description'
+      )->nullable();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('clubs');
+  }
+};
