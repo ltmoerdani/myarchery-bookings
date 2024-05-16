67b8e350ed457cb5d3883b36796d443a2b977974
ginadwi <ginadwi.official@gmail.com>
2024-05-16 07:50:29 +0700

    Create model participant

---
+++ app/Models/Participant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Participant extends Model
+{
+    use HasFactory;
+    protected $table = 'participant';
+    protected $fillable = [
+        'fname',
+        'lname',
+        'gender',
+        'birthdate',
+        'county_id',
+        'country',
+        'city_id',
+        'city',
+        'category',
+        'club_id',
+        'customer_id',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s'
+    ];
+}
