4723e1871436a98355581145d6caa0cef2fc6948
faizadie30 <faiz.adie30@gmail.com>
2024-06-12 15:43:44 +0700

    add field idcity, idstate, idcity in table event_contents

---
+++ database/migrations/2024_06_12_153217_add_field_idcountry_idstate_idcity_in_event_contents.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('event_contents', function (Blueprint $table) {
+      $table->bigInteger('country_id')->nullable()->after('address');
+      $table->bigInteger('state_id')->nullable()->after('country');
+      $table->bigInteger('city_id')->nullable()->after('state');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('event_contents', function (Blueprint $table) {
+      $table->dropColumn('country_id');
+      $table->dropColumn('state_id');
+      $table->dropColumn('city_id');
+    });
+  }
+};
