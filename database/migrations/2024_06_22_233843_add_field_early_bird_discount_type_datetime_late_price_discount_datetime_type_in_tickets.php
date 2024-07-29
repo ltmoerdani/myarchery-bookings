d8217e2f1c09928aa497fdd33a4b0678b437d177
faizadie30 <faiz.adie30@gmail.com>
2024-07-29 11:31:51 +0700

    remove after in migration

--- database/migrations/2024_06_22_233843_add_field_early_bird_discount_type_datetime_late_price_discount_datetime_type_in_tickets.php
+++ database/migrations/2024_06_22_233843_add_field_early_bird_discount_type_datetime_late_price_discount_datetime_type_in_tickets.php
@@ -20,24 +20,24 @@ return new class extends Migration
       $table->string('early_bird_discount_time')->nullable()->comment('use for start time local earlybird tournament and early bird time outer tournament event')->change();
       $table->string('early_bird_discount_end_date')->nullable()->comment('user for local end date early bird tournament and support use outer tournament event');
       $table->string('early_bird_discount_end_time')->nullable()->comment('user for local end time early bird tournament and support use outer tournament event');
-      $table->string('early_bird_discount_amount_international')->nullable()->after('early_bird_discount_end_time')->change();
+      $table->string('early_bird_discount_amount_international')->nullable()->change();
       $table->string('early_bird_discount_international_type')->nullable()->comment('use for type international earlybird tournament');
       $table->string('early_bird_discount_international_date')->nullable()->comment('use for start date international earlybird tournament and support if use outer tournament event');
       $table->string('early_bird_discount_international_time')->nullable()->comment('use for start time international earlybird tournament and support if use outer tournament event');
       $table->string('early_bird_discount_international_end_date')->nullable()->comment('use for end date international earlybird tournament and support if use outer tournament event');
       $table->string('early_bird_discount_international_end_time')->nullable()->comment('use for end time international earlybird tournament and support if use outer tournament event');
       $table->string('late_price_discount_amount')->nullable()->comment('use for local late price tournament and support for outer tournament event')->change();
-      $table->string('late_price_discount_type')->nullable()->after('late_price_discount_amount')->comment('use for local late price tournament and support for outer tournament event')->change();
-      $table->string('late_price_discount_date')->nullable()->after('late_price_discount_type')->comment('use for local start date late price tournament and support for outer tournament event')->change();
-      $table->string('late_price_discount_time')->nullable()->after('late_price_discount_date')->comment('use for local start time late price tournament and support for outer tournament event')->change();
-      $table->string('late_price_discount_end_date')->nullable()->after('late_price_discount_time')->comment('use for local end date late price tournament and support for outer tournament event');
-      $table->string('late_price_discount_end_time')->nullable()->after('late_price_discount_end_date')->comment('use for local end time late price tournament and support for outer tournament event');
-      $table->string('late_price_discount_amount_international')->nullable()->after('late_price_discount_end_time')->comment('use for international late price tournament and support for outer tournament event')->change();
-      $table->string('late_price_discount_international_type')->nullable()->after('late_price_discount_amount_international')->comment('use for international late price tournament and support for outer tournament event');
-      $table->string('late_price_discount_international_date')->nullable()->after('late_price_discount_international_type')->comment('use for start date international late price tournament and support for outer tournament event');
-      $table->string('late_price_discount_international_time')->nullable()->after('late_price_discount_international_date')->comment('use for start time international late price tournament and support for outer tournament event');
-      $table->string('late_price_discount_international_end_date')->nullable()->after('late_price_discount_international_time')->comment('use for end date international late price tournament and support for outer tournament event');
-      $table->string('late_price_discount_international_end_time')->nullable()->after('late_price_discount_international_end_date')->comment('use for end time international late price tournament and support for outer tournament event');
+      $table->string('late_price_discount_type')->nullable()->comment('use for local late price tournament and support for outer tournament event')->change();
+      $table->string('late_price_discount_date')->nullable()->comment('use for local start date late price tournament and support for outer tournament event')->change();
+      $table->string('late_price_discount_time')->nullable()->comment('use for local start time late price tournament and support for outer tournament event')->change();
+      $table->string('late_price_discount_end_date')->nullable()->comment('use for local end date late price tournament and support for outer tournament event');
+      $table->string('late_price_discount_end_time')->nullable()->comment('use for local end time late price tournament and support for outer tournament event');
+      $table->string('late_price_discount_amount_international')->nullable()->comment('use for international late price tournament and support for outer tournament event')->change();
+      $table->string('late_price_discount_international_type')->nullable()->comment('use for international late price tournament and support for outer tournament event');
+      $table->string('late_price_discount_international_date')->nullable()->comment('use for start date international late price tournament and support for outer tournament event');
+      $table->string('late_price_discount_international_time')->nullable()->comment('use for start time international late price tournament and support for outer tournament event');
+      $table->string('late_price_discount_international_end_date')->nullable()->comment('use for end date international late price tournament and support for outer tournament event');
+      $table->string('late_price_discount_international_end_time')->nullable()->comment('use for end time international late price tournament and support for outer tournament event');
     });
   }
 
@@ -50,16 +50,16 @@ return new class extends Migration
   {
     Schema::table('tickets', function (Blueprint $table) {
       // change column
-      $table->string('early_bird_discount_amount')->nullable()->after('early_bird_discount')->change();
-      $table->string('early_bird_discount_amount_international')->nullable()->after('early_bird_discount_amount')->change();
-      $table->string('early_bird_discount_type')->nullable()->after('early_bird_discount_amount_international')->change();
-      $table->string('early_bird_discount_date')->nullable()->after('early_bird_discount_type')->change();
-      $table->string('early_bird_discount_time')->nullable()->after('early_bird_discount_date')->change();
-      $table->string('late_price_discount_amount')->nullable()->after('late_price_discount')->change();
-      $table->string('late_price_discount_amount_international')->nullable()->after('late_price_discount_amount')->change();
-      $table->string('late_price_discount_type')->nullable()->after('late_price_discount_amount_international')->change();
-      $table->string('late_price_discount_date')->nullable()->after('late_price_discount_type')->change();
-      $table->string('late_price_discount_time')->nullable()->after('late_price_discount_date')->change();
+      $table->string('early_bird_discount_amount')->nullable()->change();
+      $table->string('early_bird_discount_amount_international')->nullable()->change();
+      $table->string('early_bird_discount_type')->nullable()->change();
+      $table->string('early_bird_discount_date')->nullable()->change();
+      $table->string('early_bird_discount_time')->nullable()->change();
+      $table->string('late_price_discount_amount')->nullable()->change();
+      $table->string('late_price_discount_amount_international')->nullable()->change();
+      $table->string('late_price_discount_type')->nullable()->change();
+      $table->string('late_price_discount_date')->nullable()->change();
+      $table->string('late_price_discount_time')->nullable()->change();
 
       // drop column
       $table->dropColumn('early_bird_discount_end_time');
