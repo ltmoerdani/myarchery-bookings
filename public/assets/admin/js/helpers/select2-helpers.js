bbfce17c65a682b630d8a403c33350fa966ad045
faizadie30 <faiz.adie30@gmail.com>
2024-09-17 12:01:18 +0700

    issue bug select2

--- public/assets/admin/js/helpers/select2-helpers.js
+++ public/assets/admin/js/helpers/select2-helpers.js
@@ -1,110 +1,6 @@
 /**
  * Initiate S2 With Dynamic Option Creation
  */
-// function initiateSelect2DynamicOptionCreation(
-//   elId,
-//   url,
-//   minimumInputLength = 3,
-//   placeholder = "Masukan Pilihan",
-//   attrs,
-//   onSelect,
-//   paramsCallback = null,
-//   parentModal = null,
-//   multiple = false
-// ) {
-//   return $(elId)
-//     .select2({
-//       ajax: {
-//         url: url,
-//         data:
-//           paramsCallback ??
-//           function (params) {
-//             let req = {
-//               q: params.term,
-//             };
-//             return req;
-//           },
-//         processResults: function (data) {
-//           return {
-//             results: data,
-//           };
-//         },
-//       },
-//       width: "resolve",
-//       minimumInputLength: minimumInputLength,
-//       dropdownParent: parentModal,
-//       placeholder: placeholder,
-//       multiple: multiple,
-//       templateResult: function (data) {
-//         var optText = "";
-//         if (data.loading) {
-//           return "Mencari...";
-//         }
-
-//         attrs.forEach((item, i) => {
-//           let label = item.split(".");
-//           let obj = data;
-//           if (i !== 0) {
-//             optText += " - ";
-//           }
-//           label.forEach((text) => {
-//             obj = obj[text];
-//           });
-//           optText += obj;
-//         });
-//         return optText === undefined ? data.text : optText;
-//       },
-//       templateSelection: function (data) {
-//         var optText = "";
-//         if (!data.text) {
-//           attrs.forEach((item, i) => {
-//             let label = item.split(".");
-//             let obj = data;
-//             if (i !== 0) {
-//               optText += " - ";
-//             }
-//             label.forEach((text) => {
-//               obj = obj[text];
-//             });
-//             optText += obj;
-//           });
-//           return optText;
-//         } else {
-//           return data.text;
-//         }
-//       },
-//       tags: true,
-//       createTag: function (params) {
-//         var term = $.trim(params.term);
-//         if (term === "") {
-//           return null;
-//         }
-//         return {
-//           id: term,
-//           text: term,
-//           newTag: true,
-//         };
-//       },
-//       insertTag: function (data, tag) {
-//         // Insert the tag at the end of the results
-//         data.push(tag);
-//       },
-//     })
-//     .on("select2:select", function (e) {
-//       const selectedData = e.params.data;
-
-//       // Menangani tag baru yang dipilih
-//       if (selectedData.newTag) {
-//         console.info("new tag:", selectedData);
-//       }
-
-//       // Menjalankan callback jika disediakan
-//       if (onSelect) onSelect(e);
-//     });
-// }
-/**
- * Initiate S2 With Dynamic Option Creation
- */
 function initiateSelect2DynamicOptionCreation(
   elId,
   url,
@@ -140,38 +36,27 @@ function initiateSelect2DynamicOptionCreation(
       placeholder: placeholder,
       multiple: multiple,
       templateResult: function (data) {
+        var optText = "";
         if (data.loading) {
           return "Mencari...";
         }
 
-        var optText = "";
-
-        // Tangani opsi baru
-        if (data.newTag) {
-          optText = data.text;
-        } else {
-          attrs.forEach((item, i) => {
-            let label = item.split(".");
-            let obj = data;
-            if (i !== 0) {
-              optText += " - ";
-            }
-            label.forEach((text) => {
-              obj = obj[text];
-            });
-            optText += obj;
+        attrs.forEach((item, i) => {
+          let label = item.split(".");
+          let obj = data;
+          if (i !== 0) {
+            optText += " - ";
+          }
+          label.forEach((text) => {
+            obj = obj[text];
           });
-        }
-
+          optText += obj;
+        });
         return optText === undefined ? data.text : optText;
       },
       templateSelection: function (data) {
         var optText = "";
-
-        // Tangani opsi baru
-        if (data.newTag) {
-          optText = data.text;
-        } else if (!data.text) {
+        if (!data.text) {
           attrs.forEach((item, i) => {
             let label = item.split(".");
             let obj = data;
@@ -183,11 +68,10 @@ function initiateSelect2DynamicOptionCreation(
             });
             optText += obj;
           });
+          return optText;
         } else {
-          optText = data.text;
+          return data.text;
         }
-
-        return optText;
       },
       tags: true,
       createTag: function (params) {
@@ -195,28 +79,24 @@ function initiateSelect2DynamicOptionCreation(
         if (term === "") {
           return null;
         }
-
-        let newData = {
+        return {
           id: term,
           text: term,
           newTag: true,
         };
-
-        // Tambahkan atribut yang diperlukan berdasarkan `attrs`
-        attrs.forEach((attr) => {
-          newData[attr] = term;
-        });
-
-        return newData;
       },
       insertTag: function (data, tag) {
-        // Masukkan tag di akhir hasil
+        // Insert the tag at the end of the results
         data.push(tag);
       },
     })
     .on("select2:select", function (e) {
       const selectedData = e.params.data;
-      console.log("Event select2:select terpicu dengan data:", selectedData);
+
+      // Menangani tag baru yang dipilih
+      if (selectedData.newTag) {
+        console.info("new tag:", selectedData);
+      }
 
       // Menjalankan callback jika disediakan
       if (onSelect) onSelect(e);
